86c6e3ed0eb94651f4925226010fa713f05d67d4
Gokul Natesan <gnatesan@mines.edu>
2015-04-29 14:03:53 -0600

    Finished ConvertToBin test

--- src/Compiler/JackAnalyzer.java
+++ src/Compiler/JackAnalyzer.java
@@ -6,16 +6,18 @@ import java.io.File;
 //with the parameter as the name of the jack file
 public class JackAnalyzer {
 	
-	public static void main (String[] args) throws Exception {
-			File dir = new File("Z:/Elements/Compiler/Seven");
+	public static void main (String[] args) throws Exception { //Z:/Elements/
+			File dir = new File("C:/Compiler/ConvertToBin");
 			if (dir.isFile()) {
+				System.out.println("asdsdsdff");
 				CompilationEngine cs = new CompilationEngine (dir.getName());
 			}
 			else if (dir.isDirectory()) {
+				System.out.println("asdf");
 				for (File f : dir.listFiles()) {
 					if (f.getName().endsWith(".jack")) {
-						System.out.println(f.getName());
-						CompilationEngine cs = new CompilationEngine (f.getName());
+						System.out.println("d" + f.getName() + "d");
+						CompilationEngine cs = new CompilationEngine ("C:/Compiler/ConvertToBin/" + f.getName());
 					}
 				}
 			}
